9b9ef46614eacc0f53c207a9b87deaa76d7b93d5
PhuTam54 <phutamytb@gmail.com>
2023-04-27 21:10:25 +0700

    Update assignment 2

--- src/Assignment2/Fraction.java
+++ src/Assignment2/Fraction.java
@@ -1,8 +1,7 @@
 package Assignment2;
 
 public class Fraction {
-    int numerator;
-    int denominator;
+    int numerator, denominator;
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
@@ -25,12 +24,20 @@ public class Fraction {
         this.denominator = denominator;
     }
 
-    //in phan so
+    /**
+     * In phân số
+     */
     void printFraction() {
         System.out.println(this.numerator + "/" + this.denominator);
     }
 
-    //rut gon phan so
+    /**
+     * // Rút gọn phân số
+     *
+     * checkGreatestMuliple
+     * compactFraction
+     */
+
     int checkGreatestMuliple(int numerator, int denominator) {
         int x = numerator;
         int y = denominator;
@@ -50,45 +57,57 @@ public class Fraction {
         this.setDenominator(this.getDenominator() / i);
     }
 
-    //nghich dao phan so
-    void inverseFraction() {
+    /**
+     * Nghịch đảo phân số
+     */
 
+    void inverseFraction() {
+        int temp = this.numerator;
+        this.numerator = this.denominator;
+        this.denominator = temp;
     }
 
-    // Cong 2 phan so
-    void addFraction(Fraction fr) {
+    /**
+     * Cộng 2 phân số
+     */
+    Fraction addFraction(Fraction fr) {
         int numer = this.getNumerator() * fr.getDenominator() + fr.getNumerator() * this.getDenominator();
         int deno = this.getDenominator() * fr.getDenominator();
         Fraction totalFr = new Fraction(numer, deno);
         totalFr.compactFraction();
-        System.out.println("Sum of 2 fractions = " + totalFr.numerator + "/" + totalFr.denominator);
+        return totalFr;
     }
 
-    // Tru 2 phan so
-    void subFraction(Fraction fr) {
+    /**
+     * Trừ 2 phân số
+     */
+    Fraction subFraction(Fraction fr) {
         int numer = this.getNumerator() * fr.getDenominator() - fr.getNumerator() * this.getDenominator();
         int deno = this.getDenominator() * fr.getDenominator();
         Fraction differenceFr = new Fraction(numer, deno);
         differenceFr.compactFraction();
-        System.out.println("Difference of 2 fractions = " + differenceFr.numerator + "/" + differenceFr.denominator);
+        return differenceFr;
     }
 
-    // Nhan 2 phan so
-    void mulFraction(Fraction fr) {
+    /**
+     * Nhân 2 phân số
+     */
+    Fraction mulFraction(Fraction fr) {
         int numer = this.getNumerator() * fr.getNumerator();
         int deno = this.getDenominator() * fr.getDenominator();
         Fraction productFr = new Fraction(numer, deno);
         productFr.compactFraction();
-        System.out.println("Product of 2 fractions = " + productFr.numerator + "/" + productFr.denominator);
+        return productFr;
     }
 
-    // Chia 2 phan so
-    void divFraction(Fraction fr) {
+    /**
+     * Chia 2 phân số
+     */
+    Fraction divFraction(Fraction fr) {
         int numer = this.getNumerator() * fr.getDenominator();
         int deno = this.getDenominator() * fr.getNumerator();
         Fraction quotientFr = new Fraction(numer, deno);
         quotientFr.compactFraction();
-        System.out.println("Product of 2 fractions = " + quotientFr.numerator + "/" + quotientFr.denominator);
+        return quotientFr;
     }
-
 }
--- src/Assignment2/Main.java
+++ src/Assignment2/Main.java
@@ -2,14 +2,31 @@ package Assignment2;
 
 public class Main {
     public static void main(String[] args) {
-        Fraction a = new Fraction(2, 2);
-        Fraction b = new Fraction(8, 2);
+        Fraction a = new Fraction(3, 2);
+        Fraction b = new Fraction(8, 15);
+
+        System.out.println("Fraction: ");
         a.printFraction();
         b.printFraction();
 
-        a.addFraction(b);
-        b.subFraction(a);
-        a.mulFraction(b);
-        a.divFraction(b);
+        System.out.println("Sum of 2 fractions: ");
+        a.addFraction(b).printFraction();
+
+        System.out.println("Difference of 2 fractions: ");
+        a.subFraction(b).printFraction();
+
+        System.out.println("Product of 2 fractions: ");
+        a.mulFraction(b).printFraction();
+
+        System.out.println("Quotient of 2 fractions: ");
+        a.divFraction(b).printFraction();
+
+        System.out.println("Inverse of fraction a: ");
+        a.inverseFraction();
+        a.printFraction();
+
+        System.out.println("Inverse of fraction b: ");
+        b.inverseFraction();
+        b.printFraction();
     }
 }
