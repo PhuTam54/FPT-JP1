f2e937a0a09980cb76030fe7256e27dada95d410
PhuTam54 <phutamytb@gmail.com>
2023-05-25 15:03:34 +0700

    Add practical exam JP1

---
+++ src/practicalexam/BookDemo.java
@@ -0,0 +1,9 @@
+package practicalexam;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Author author = new Author("Russel", "Winderand");
+        Book book = new Book("Developing Java Software", author, 79.75);
+        System.out.println(book);
+    }
+}
