e956fbe1b92b907ea81ee005ba956b77f128eaad
PhuTam54 <phutamytb@gmail.com>
2023-05-28 23:34:17 +0700

    update assignment 4

--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -1,6 +1,5 @@
 package assignment4;
 
-import Practice.Student;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/assignment4/Person.java
+++ src/assignment4/Person.java
@@ -1,7 +1,5 @@
 package assignment4;
 
-import Practice.Student;
-
 import java.util.List;
 import java.util.Scanner;
 
